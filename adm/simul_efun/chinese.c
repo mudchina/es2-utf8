4335f92ce67057903365cd6f36212985c94554d6
floatinghotpot <rjfun.mobile@gmail.com>
2016-02-23 10:56:33 +0800

    Add simul efun utf8_strlen()

--- adm/simul_efun/chinese.c
+++ adm/simul_efun/chinese.c
@@ -14,3 +14,14 @@ int is_chinese(string str)
 	if( strlen(str)>=2 && str[0] > 128 && str[0] < 255 ) return 1;
 	return 0;
 }
+
+int utf8_strlen(string str)
+{
+  int i;
+  int n = 0;
+  int len = strlen(str);
+  for(i=0; i<len; i++) {
+    if ((str[i] & 0xC0) != 0x80) n++;
+  }
+  return n;
+}
