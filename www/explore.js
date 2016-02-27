478f8d94d94e4b1a56655ac52d6fb5f1cbc92b04
Liming Xie <liming.xie@gmail.com>
2016-02-28 01:42:16 +0800

    Add target links to touch.
    Any (xxx) info in text of room, character, item, inventory will be linked.
    When move to new room, old links removed.

---
+++ www/explore.js
@@ -0,0 +1,160 @@
+var ROOM_MARK = '▲ ', CHAR_MARK = '▼ ', ITEM_MARK = '◆ ';
+
+var EXITS_MARK = '这里明显的出口是';
+var EXITS_MASK2 = '这里唯一的出口是';
+var ESC_CHAR = String.fromCharCode(27);
+var ESC_1 = new RegExp(ESC_CHAR+'\\[1m', 'g');
+var ESC_2 = new RegExp(ESC_CHAR+'\\[2;37;0m', 'g');
+
+function addTargetLinks(str) {
+  var targets = str.match(/\([\w ]+\)/gi);
+  if(targets) {
+    targets = _.unique(targets);
+    for(var i=0; i<targets.length; i++) {
+      var quote_word = targets[i];
+      var word = quote_word.replace('(','').replace(')','');
+      var id = word.split(' ')[0].toLowerCase();
+      var link_word = '(<a class="cmd" href="#" macro="l ' + id + '">' + word + '</a>)';
+      str = str.replace(new RegExp('\\(' + word + '\\)', 'gi'), link_word);
+    }
+  }
+  return str;
+}
+
+// copy from es2 mudlib: /cmds/std/go.c
+var allDirs = {
+  'north': ['n', '北'],
+  'south': ['s', '南'],
+  'east': ['e', '东'],
+  'west': ['w', '西'],
+  'northup': ['n', '北上'],
+  'southup': ['s', '南上'],
+  'eastup': ['e', '东上'],
+  'westup': ['w', '西上'],
+  'northdown': ['n', '北下'],
+  'southdown': ['s', '南下'],
+  'eastdown': ['e', '东下'],
+  'westdown': ['w', '西下'],
+  'northeast': ['ne', '东北'],
+  'northwest': ['nw', '西北'],
+  'southeast': ['se', '东南'],
+  'southwest': ['sw', '西南'],
+  'up': ['nw', '上'],
+  'down': ['sw', '下'],
+  'out': ['ne', '外'],
+  'enter': ['ne', '里'],
+};
+var SHORT_DIRS = ['n','s','e','w','nw','sw','ne','se'];
+
+function parseExits(str) {
+  // parse exits, enable / disable go keys
+  var marks = str.split(ROOM_MARK);
+  for(var i=1; i<marks.length; i++) {
+    var desc = marks[i];
+    var p = desc.indexOf(EXITS_MARK);
+    if(p < 0) {
+      p = desc.indexOf(EXITS_MASK2);
+      if(p < 0) continue;
+    }
+    p += EXITS_MARK.length;
+    var p2 = desc.indexOf('。', p);
+    if(p2 < 0) continue;
+
+    // extra open exits to array
+    var exits = desc.substr(p, p2-p)
+      .replace(ESC_1, '').replace(ESC_2, '').replace(/、/g, ',').replace('和', ',')
+      .split(',');
+    for(var j=0; j<exits.length; j++) {
+      exits[j] = exits[j].trim();
+    }
+
+    var openDirs = {};
+    for(var j=0; j<SHORT_DIRS.length; j++) {
+      openDirs[ SHORT_DIRS[j] ] = 0;
+    }
+    var domGo = $('table#go');
+    for(var j=0; j<exits.length; j++) {
+      var dir = exits[j];
+      var dirInfo = allDirs[ dir ];
+      if(!dirInfo) continue;
+
+      // enable go keys for valid exits
+      var dirShort = dirInfo[0];
+      var dirName = dirInfo[1];
+      $('button#'+dirShort, domGo).attr('macro',dir).text(dirName).removeClass('disable');
+      openDirs[ dirShort ] = 1;
+    }
+    // disable go keys for invalid exits
+    for(var k in openDirs) {
+      var btn = $('button#'+k, domGo);
+      if(!openDirs[k]) btn.attr('macro','').text('').addClass('disable');
+    }
+  }
+}
+
+function parseRoom(str) {
+  // when we move or look room, we reset target char & item
+  setTargetChar('');
+  setTargetItem('');
+  
+  // when new room info comes, we remove old links
+  $('div#out').find('a').contents().unwrap();
+
+  parseExits(str);
+
+  return addTargetLinks(str);
+}
+
+// TODO: find NPC id,
+// 1. set as default target
+// 2. show context-sensitive commands
+function parseChar(str) {
+  // when we look at char, we reset targt item
+  setTargetItem('');
+
+  var marks = str.split(CHAR_MARK);
+  for(var i=1; i<marks.length; i++) {
+    var desc = marks[i];
+    var targets = desc.match(/\([\w ]+\)/gi);
+    if(targets) {
+      var word = targets[0].replace('(','').replace(')','');
+      var id = word.split(' ')[0].toLowerCase();
+      setTargetChar(id);
+    }
+  }
+  
+  return addTargetLinks(str);
+}
+
+// TODO: find Item id,
+// 1. set as default target
+// 2. show context-sensitive commands
+function parseItem(str) {
+  return addTargetLinks(str);
+}
+
+var exploreCmds = [
+  ['捡', 'get $item'],
+  ['放', 'put'],
+  ['给', 'give $item to $char'],
+  ['丢', 'drop $item'],
+  ['买', 'buy $item from $char'],
+  ['偷', 'steal $item from $char'],
+  ['开门', 'open door\nl'],
+  ['关门', 'close door\nl'],
+  ['挑战', 'fight $char'],
+  ['杀', 'kill $char'],
+  ['跟随', 'follow $char'],
+  ['组队', 'team $char'],
+  ['状态', 'hp $char'],
+  ['物品', 'i'],
+  ['技能', 'skills $char'],
+  ['成就', 'score $char'],
+];
+
+function initExploreKeys(callback) {
+  $('button.go').click(callback);
+  initKeys(exploreCmds, 'div#expkeys', callback);
+}
+
+
--- www/main.js
+++ www/main.js
@@ -1,113 +1,16 @@
-var ROOM_MARK = '▲ ', CHAR_MARK = '▼ ', ITEM_MARK = '◆ ';
-
-var EXITS_MARK = '这里明显的出口是';
-var EXITS_MASK2 = '这里唯一的出口是';
-var ESC_CHAR = String.fromCharCode(27);
-var ESC_1 = new RegExp(ESC_CHAR+'\\[1m', 'g');
-var ESC_2 = new RegExp(ESC_CHAR+'\\[2;37;0m', 'g');
-
-// copy from es2 mudlib: /cmds/std/go.c
-var allDirs = {
-  'north': ['n', '北'],
-  'south': ['s', '南'],
-  'east': ['e', '东'],
-  'west': ['w', '西'],
-  'northup': ['n', '北上'],
-  'southup': ['s', '南上'],
-  'eastup': ['e', '东上'],
-  'westup': ['w', '西上'],
-  'northdown': ['n', '北下'],
-  'southdown': ['s', '南下'],
-  'eastdown': ['e', '东下'],
-  'westdown': ['w', '西下'],
-  'northeast': ['ne', '东北'],
-  'northwest': ['nw', '西北'],
-  'southeast': ['se', '东南'],
-  'southwest': ['sw', '西南'],
-  'up': ['nw', '上'],
-  'down': ['sw', '下'],
-  'out': ['ne', '外'],
-  'enter': ['ne', '里'],
-};
-var SHORT_DIRS = ['n','s','e','w','nw','sw','ne','se'];
-
-// parse exits, enable / disable go keys
-function parseRoom(str) {
-  var marks = str.split(ROOM_MARK);
-  for(var i=1; i<marks.length; i++) {
-    var desc = marks[i];
-    var p = desc.indexOf(EXITS_MARK);
-    if(p < 0) {
-      p = desc.indexOf(EXITS_MASK2);
-      if(p < 0) continue;
-    }
-    p += EXITS_MARK.length;
-    var p2 = desc.indexOf('。', p);
-    if(p2 < 0) continue;
-
-    // extra open exits to array
-    var exits = desc.substr(p, p2-p)
-      .replace(ESC_1, '').replace(ESC_2, '').replace(/、/g, ',').replace('和', ',')
-      .split(',');
-    for(var j=0; j<exits.length; j++) {
-      exits[j] = exits[j].trim();
-    }
-
-    var openDirs = {};
-    for(var j=0; j<SHORT_DIRS.length; j++) {
-      openDirs[ SHORT_DIRS[j] ] = 0;
-    }
-    var domGo = $('table#go');
-    for(var j=0; j<exits.length; j++) {
-      var dir = exits[j];
-      var dirInfo = allDirs[ dir ];
-      if(!dirInfo) continue;
-
-      var dirShort = dirInfo[0];
-      var dirName = dirInfo[1];
-      openDirs[ dirShort ] = 1;
-      $('button#'+dirShort, domGo).attr('macro',dir).text(dirName).removeClass('disable');
-    }
-    for(var k in openDirs) {
-      var btn = $('button#'+k, domGo);
-      if(!openDirs[k]) btn.attr('macro','').text('').addClass('disable');
-    }
-  }
-  return str;
-}
-
-// TODO: find NPC id,
-// 1. set as default target
-// 2. show context-sensitive commands
-function parseChar(str) {
-  var marks = str.split(CHAR_MARK);
-  for(var i=1; i<marks.length; i++) {
-    var desc = marks[i];
-  }
-  return str;
-}
-
-// TODO: find Item id,
-// 1. set as default target
-// 2. show context-sensitive commands
-function parseItem(str) {
-  var marks = str.split(ITEM_MARK);
-  for(var i=1; i<marks.length; i++) {
-    var desc = marks[i];
-  }
-  return str;
-}
-
 function scrollDown() {
   var out = $('div#out');
   out.scrollTop(out.prop('scrollHeight'));
 }
 
 function writeToScreen(str) {
-  $('div#out').append('<span class="out">' + str + '</span>');
+  var p = $('<p>').append(str).addClass('out');
+  p.appendTo('div#out');
+  $('a.cmd', p).on('click', onMacroKey);
   scrollDown();
 }
 
+var ROOM_MARK = '▲ ', CHAR_MARK = '▼ ', ITEM_MARK = '◆ ', INV_MARK = '带著下列这些东西';
 var LOGIN_MARK = '您的英文名字：', PASS_MARK = '请输入密码：';
 var askingLogin = false, askingPass = false, autologin = false;
 
@@ -115,11 +18,13 @@ function writeServerData(buf) {
   var data = new Uint8Array(buf);
   var str = binayUtf8ToString(data, 0);
 
+  askingLogin = askingPass = false;
   if(str.indexOf(ROOM_MARK) >= 0) str = parseRoom(str);
-  if(str.indexOf(CHAR_MARK) >= 0) str = parseChar(str);
-  if(str.indexOf(ITEM_MARK) >= 0) str = parseItem(str);
-  askingLogin = (str.indexOf(LOGIN_MARK) >= 0);
-  askingPass = (str.indexOf(PASS_MARK) >= 0);
+  else if(str.indexOf(CHAR_MARK) >= 0) str = parseChar(str);
+  else if(str.indexOf(ITEM_MARK) >= 0) str = parseItem(str);
+  else if(str.indexOf(INV_MARK) >= 0) str = addTargetLinks(str);
+  else if(str.indexOf(LOGIN_MARK) >= 0) askingLogin = true;
+  else if(str.indexOf(PASS_MARK) >= 0) askingPass = true;
   
   var lines = str.split('\r\n');
   for(var i=0; i<lines.length; i++) {
@@ -141,37 +46,62 @@ function writeServerData(buf) {
       if(autologin) autologin = confirm('是否用记住的密码自动登录？');
       if(autologin) {
         var u = localStorage.getItem('username');
-        if(sendData && u) sendData(u + '\n');
+        if(sendCmd && u) sendCmd(u);
       }
     }, 100);
   }
   if(askingPass) {
     if(autologin) {
       var p = localStorage.getItem('password');
-      if(sendData && p) sendData(p + '\n');
+      if(sendCmd && p) sendCmd(p);
       $('button#explore').click();
     }
   }
 }
 
-var exploreCmds = [
-  ['捡', 'get'],
-  ['放', 'put'],
-  ['给', 'give'],
-  ['丢', 'drop'],
-  ['买', 'buy'],
-  ['偷', 'steal'],
-  ['开门', 'open door'],
-  ['关门', 'close door'],
-  ['挑战', 'fight'],
-  ['杀', 'kill'],
-  ['跟随', 'follow'],
-  ['组队', 'team'],
-  ['状态', 'hp'],
-  ['物品', 'i'],
-  ['技能', 'skills'],
-  ['成就', 'score'],
-];
+var itemTarget = '', charTarget = '';
+
+function setTargetItem(str) {
+  itemTarget = str;
+  $('span.itemtarget').text('物品目标：'+str);
+}
+
+function setTargetChar(str) {
+  charTarget = str;
+  $('span.chartarget').text('角色目标：'+str);
+}
+
+function connectServer() {
+  // websocket
+  var sock = io.connect();
+  sock.on('data', function(buf){
+    writeServerData(buf);
+  });
+  sock.on('status', function(str){
+    writeToScreen(str);
+  });
+  sock.on('connected', function(){
+    console.log('connected');
+  });
+  sock.on('disconnect', function(){
+    console.log('disconnected');
+  });
+
+  // send
+  window.sendCmd = function(str) {
+    if(str.indexOf('$char')) str = str.replace('$char', charTarget);
+    if(str.indexOf('$item')) str = str.replace('$item', itemTarget);
+    str = str.trim();
+
+    // when we look at sth, we set it as item target
+    if((str.indexOf('l ') >= 0) || (str.indexOf('look ') >= 0)) {
+      setTargetItem(str.split(' ')[1]);
+    }
+
+    //writeToScreen(str);
+    if(sock) sock.emit('data', str + '\n');
+  }
+}
 
 function initKeys(cmds, div, callback) {
   for(var i=0; i<cmds.length; i++) {
@@ -212,31 +142,6 @@ function showPad(name) {
   adjustLayout();
 }
 
-function connectServer() {
-  // websocket
-  var sock = io.connect();
-  sock.on('data', function(buf){
-    writeServerData(buf);
-  });
-  sock.on('status', function(str){
-    writeToScreen(str);
-  });
-  sock.on('connected', function(){
-    console.log('connected');
-  });
-  sock.on('disconnect', function(){
-    console.log('disconnected');
-  });
-
-  // send
-  window.sendData = function(str) {
-    //writeToScreen(str);
-    if(sock) sock.emit('data', str);
-  }
-}
-
-$(window).resize(adjustLayout);
-
 var cmdHistory = [];
 var cmdIndex = 0;
 
@@ -257,45 +162,54 @@ function sendInput() {
   }
   cmdIndex = cmdHistory.length;
 
-  if(sendData) sendData(str + '\n');
+  if(sendCmd) sendCmd(str);
   cmd.val('');
+}
+
+function cmdHistoryUp() {
+  if(cmdIndex > 0) {
+    cmdIndex --;
+    if(cmdIndex < cmdHistory.length) {
+      var str = cmdHistory[cmdIndex];
+      $('input#str').val(str);
+    }
+  } else $('input#str').val('');
+}
+
+function cmdHistoryDown() {
+  if(cmdIndex < cmdHistory.length-1) {
+    cmdIndex ++;
+    var str = cmdHistory[cmdIndex];
+    $('input#str').val(str);
+  } else $('input#str').val('');
 }
 
 function onMacroKey(e) {
   var me = $(e.currentTarget);
   var str = me.attr('macro');
-  if(sendData) sendData(str + '\n');
+  //console.log(str);
+  if(sendCmd && str) sendCmd(str);
 }
 
 function initUI() {
+  $(window).resize(adjustLayout);
+
   $('button.menu').click(function(e) {
     var name = $(e.currentTarget).attr('id');
     showPad(name);
   });
 
-  $('button.go').click(onMacroKey);
-
-  initKeys(exploreCmds, 'div#expkeys', onMacroKey);
+  initExploreKeys(onMacroKey);
 
   // UI events
   $('input#str').keydown(function(e) {
     // console.log(e.keyCode);
     switch(e.keyCode) {
       case 38: // up arrow key
-        if(cmdIndex > 0) {
-          cmdIndex --;
-          if(cmdIndex < cmdHistory.length) {
-            var str = cmdHistory[cmdIndex];
-            $('input#str').val(str);
-          }
-        } else $('input#str').val('');
+        cmdHistoryUp();
         break;
       case 40: // down arrow key
-        if(cmdIndex < cmdHistory.length-1) {
-          cmdIndex ++;
-          var str = cmdHistory[cmdIndex];
-          $('input#str').val(str);
-        } else $('input#str').val('');
+        cmdHistoryDown();
         break;
       case 13: // enter
         sendInput();
